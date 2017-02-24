c54cca6bb1038ce5f79be71c5443b34b4ff8b67a
Georg Eitler <funer@gmx.at>
2017-02-24 15:08:43 +0100

    Use Parameters along with argument strings or custom values
    Also kick the priority and strings keys

--- heal.lua
+++ heal.lua
@@ -1,9 +1,20 @@
 --// Heal Module //--
 
 StyleMeter.module.Heal = {
-	["priority"] = 2,
-	["strings"] = {
-		"_HEAL"
+	["_HEAL"] = {
+		["spellId"] = "arg12",
+		["spellName"] = "arg13",
+		["spellSchool"] = "arg14",
+		["amount"] = "arg15",
+		["overkill"] = "arg16",
+		["school"] = "arg17",
+		["resisted"] = "arg18",
+		["blocked"] = "arg19",
+		["absorbed"] = "arg20",
+		["critical"] = "arg21",
+		["glancing"] = "arg22",
+		["crushing"] = "arg23",
+		["isOffHand"] = "arg24",
 	},
 }
 
