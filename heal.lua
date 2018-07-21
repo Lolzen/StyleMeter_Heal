e345d3cc61c2fa6b74a8eeed13a5b46c0aded5b1
Georg Eitler <funer@gmx.at>
2018-07-21 20:51:31 +0200

    heal: do not differntiate between _HEAL suffixes
    but still ship fallback, just in case

--- heal.lua
+++ heal.lua
@@ -1,19 +1,58 @@
 --// Heal Module //--
 
 StyleMeter.module.Heal = {
+--	["SWING_HEAL"] = {
+--		["spellId"] = 6603, --temporary
+--		["spellName"] = "UnKnown Heal", --temporary
+--		["spellSchool"] = 2,
+--		["amount"] = "arg12",
+--		["overheal"] = "arg13",
+--		["absorbed"] = "arg14",
+--		["critical"] = "arg15",
+--	},
+--	["RANGE_HEAL"] = {
+--		["spellId"] = "arg12",
+--		["spellName"] = "arg13",
+--		["spellSchool"] = "arg14",
+--		["amount"] = "arg15",
+--		["overheal"] = "arg16",
+--		["absorbed"] = "arg17",
+--		["critical"] = "arg18",
+--	},
+--	["SPELL_HEAL"] = {
+--		["spellId"] = "arg12",
+--		["spellName"] = "arg13",
+--		["spellSchool"] = "arg14",
+--		["amount"] = "arg15",
+--		["overheal"] = "arg16",
+--		["absorbed"] = "arg17",
+--		["critical"] = "arg18",
+--	},
+--	["SPELL_PERIODIC_HEAL"] = {
+--		["spellId"] = "arg12",
+--		["spellName"] = "arg13",
+--		["spellSchool"] = "arg14",
+--		["amount"] = "arg15",
+--		["overheal"] = "arg16",
+--		["absorbed"] = "arg17",
+--		["critical"] = "arg18",
+--	},
+--	["SPELL_HEAL_ABSORBED"] = {
+--		["spellId"] = "arg12",
+--		["spellName"] = "arg13",
+--		["spellSchool"] = "arg14",
+--		["amount"] = "arg15",
+--		["overheal"] = "arg16",
+--		["absorbed"] = "arg17",
+--		["critical"] = "arg18",
+--	},
 	["_HEAL"] = {
-		["spellId"] = "arg12",
-		["spellName"] = "arg13",
-		["spellSchool"] = "arg14",
+		["spellId"] = "arg12" or 6603, --temporary
+		["spellName"] = "arg13" or "UnKnown Heal", --temporary
+		["spellSchool"] = "arg14" or 2,
 		["amount"] = "arg15",
-		["overkill"] = "arg16",
-		["school"] = "arg17",
-		["resisted"] = "arg18",
-		["blocked"] = "arg19",
-		["absorbed"] = "arg20",
-		["critical"] = "arg21",
-		["glancing"] = "arg22",
-		["crushing"] = "arg23",
-		["isOffHand"] = "arg24",
+		["overheal"] = "arg16",
+		["absorbed"] = "arg17",
+		["critical"] = "arg18",
 	},
 }
