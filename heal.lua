1fc046718bb26f1c1c533ff26a32548c37602d28
Georg Eitler <funer@gmx.at>
2018-07-21 23:00:14 +0200

    heal: simplify and clean up

--- heal.lua
+++ heal.lua
@@ -1,55 +1,10 @@
 --// Heal Module //--
 
 StyleMeter.module.Heal = {
---	["SWING_HEAL"] = {
---		["spellId"] = 6603, --temporary
---		["spellName"] = "UnKnown Heal", --temporary
---		["spellSchool"] = 2,
---		["amount"] = "arg12",
---		["overheal"] = "arg13",
---		["absorbed"] = "arg14",
---		["critical"] = "arg15",
---	},
---	["RANGE_HEAL"] = {
---		["spellId"] = "arg12",
---		["spellName"] = "arg13",
---		["spellSchool"] = "arg14",
---		["amount"] = "arg15",
---		["overheal"] = "arg16",
---		["absorbed"] = "arg17",
---		["critical"] = "arg18",
---	},
---	["SPELL_HEAL"] = {
---		["spellId"] = "arg12",
---		["spellName"] = "arg13",
---		["spellSchool"] = "arg14",
---		["amount"] = "arg15",
---		["overheal"] = "arg16",
---		["absorbed"] = "arg17",
---		["critical"] = "arg18",
---	},
---	["SPELL_PERIODIC_HEAL"] = {
---		["spellId"] = "arg12",
---		["spellName"] = "arg13",
---		["spellSchool"] = "arg14",
---		["amount"] = "arg15",
---		["overheal"] = "arg16",
---		["absorbed"] = "arg17",
---		["critical"] = "arg18",
---	},
---	["SPELL_HEAL_ABSORBED"] = {
---		["spellId"] = "arg12",
---		["spellName"] = "arg13",
---		["spellSchool"] = "arg14",
---		["amount"] = "arg15",
---		["overheal"] = "arg16",
---		["absorbed"] = "arg17",
---		["critical"] = "arg18",
---	},
 	["_HEAL"] = {
-		["spellId"] = "arg12" or 6603, --temporary
-		["spellName"] = "arg13" or "UnKnown Heal", --temporary
-		["spellSchool"] = "arg14" or 2,
+		["spellId"] = "arg12",
+		["spellName"] = "arg13",
+		["spellSchool"] = "arg14",
 		["amount"] = "arg15",
 		["overheal"] = "arg16",
 		["absorbed"] = "arg17",
